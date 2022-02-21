68df9d8a50bebeecff65368178b98a6798507331
kreywilo <kreywilo42@gmail.com>
2022-02-21 14:02:37 +0300

    logs stored in a database

--- daraja_stk/settings.py
+++ daraja_stk/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'daraja_stk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'daraja',
+        'USER':'moringa',
+        'password':'1234',
+        
     }
 }
 
