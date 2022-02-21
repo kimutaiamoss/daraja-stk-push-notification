4b755c78424b0b113a848de7f371d553c491accc
kreywilo <kreywilo42@gmail.com>
2022-02-21 13:49:43 +0300

    added index function to define logs

--- daraja1/views.py
+++ daraja1/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.conf import settings
+import logging
+import json
+from django.urls import reverse
+from .models import Log
 # Create your views here.
+#getting logger instance
+logger = logging.getLogger('daraja')
+
+
+
+def index(request):
+    logs = Log.objects.all()[:50]
+    page_context = {
+        'logs': logs
+    }
+    return render(request, 'index.html', context=page_context)
